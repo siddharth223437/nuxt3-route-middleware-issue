fdb5fc4fadb85a55bdf97602fad1818c25bbb61a
ssarathe <siddharth-sarathe@uiowa.edu>
2022-04-22 22:28:19 -0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    index page
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ plugins/myPluginl.ts
@@ -0,0 +1,6 @@
+export default defineNuxtPlugin(() => {
+    addRouteMiddleware('global-test', () => {
+        console.log('this global middleware was added in a plugin and will be run on every route change')
+    }, { global: true })
+
+})
